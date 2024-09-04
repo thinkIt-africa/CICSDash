d2d43666e59ff8363912d95581d85d45f2a658d5
JOSHUA BUSINGE <joshuabujo9@gmail.com>
2024-09-04 20:18:46 +0300

    updated files

--- streamlitapp.py
+++ streamlitapp.py
@@ -47,33 +47,33 @@ if conn:
             return []
 
     rows1 = run_query("""
-        SELECT 
+        SELECT
             qcp5_products.status AS qcp5status,
             qcp5_products.totalproductnetweight AS product_weight,
-            qcp5_products.created_at AS qcp5_timestamp 
-        FROM 
+            qcp5_products.created_at AS qcp5_timestamp
+        FROM
             qcp5_products
     """)
 
     rows2 = run_query("""
-        SELECT 
-            countries.countryname AS country,          
+        SELECT
+            countries.countryname AS country,
             clients.companyname AS clientcompany,
             packinglist_products.totalproductnetweight AS product_weight,
             packinglist_products.order_id AS order_id,
             packinglist_products.created_at AS created_at
-        FROM 
+        FROM
             packinglist_products
-        LEFT JOIN 
+        LEFT JOIN
             orders ON packinglist_products.order_id = orders.id
-        LEFT JOIN 
+        LEFT JOIN
             clients ON orders.client_id = clients.id
-        LEFT JOIN 
+        LEFT JOIN
             countries ON clients.country_id = countries.id;
     """)
 
     rows3 = run_query("""
-        SELECT 
+        SELECT
             receptionform.id AS reception_id,
             reception_attributes.qtyremoved AS reception_qtyremoved,
             qcp1_attributes.qtyremoved AS qcp1_qtyremoved,
@@ -85,29 +85,29 @@ if conn:
             crops.cropname AS cropname,
             receptionform.quantityrejected AS reception_qtyrejected,
             receptionform.created_at AS created_at
-        FROM 
+        FROM
             receptionform
-        LEFT JOIN 
+        LEFT JOIN
             reception_attributes ON reception_attributes.id = receptionform.id
-        LEFT JOIN 
+        LEFT JOIN
             qcp1_attributes ON qcp1_attributes.id = receptionform.id
-        LEFT JOIN 
+        LEFT JOIN
             qcp2_attributes ON qcp2_attributes.id = receptionform.id
-        LEFT JOIN 
+        LEFT JOIN
             qcp3_attributes ON qcp3_attributes.id = receptionform.id
-        LEFT JOIN 
+        LEFT JOIN
             qcp4_recommendations ON qcp4_recommendations.id = receptionform.id
-        LEFT JOIN 
+        LEFT JOIN
             qcp5_recommendations ON qcp5_recommendations.id = receptionform.id
-        LEFT JOIN 
+        LEFT JOIN
             exportcompanies ON exportcompanies.id = receptionform.exportcompany_id
-        LEFT JOIN 
+        LEFT JOIN
             crops ON crops.id = receptionform.crop_id;
     """)
 
     rows4 = run_query("""
         SELECT
-            receptionform.traceability_code,
+
             crops.cropname,
             receptionform.netweight,
             receptionform.created_at,
@@ -124,6 +124,30 @@ if conn:
             modesoftransport ON modesoftransport.id = receptionform.modeoftransport_id
         JOIN
             districts ON districts.id = farmer_farms.district_id;
+    """)
+
+    rows5 = run_query("""
+        SELECT
+            countries.countryname, crops.cropname, order_crops.weightreceived,
+            order_crops.weight, exportcompanies.expname AS exportcompanyname
+        FROM orders
+        LEFT JOIN exportcompanies ON exportcompanies.id = orders.exportcompany_id
+        LEFT JOIN clients ON clients.id = orders.client_id
+        LEFT JOIN countries ON countries.id = clients.country_id
+        LEFT JOIN order_crops ON order_crops.order_id = orders.id
+        LEFT JOIN crops ON crops.id = order_crops.crop_id;
+    """)
+
+    rows6 = run_query("""
+        SELECT
+            crops.cropname, order_crops.weightreceived, order_crops.weight,
+            countries.countryname
+        FROM orders
+        LEFT JOIN exportcompanies ON exportcompanies.id = orders.exportcompany_id
+        LEFT JOIN clients ON clients.id = orders.client_id
+        LEFT JOIN countries ON countries.id = clients.country_id
+        LEFT JOIN order_crops ON order_crops.order_id = orders.id
+        LEFT JOIN crops ON crops.id = order_crops.crop_id;
     """)
 
     if rows1:
@@ -170,13 +194,26 @@ if conn:
 
     if rows4:
         data4 = pd.DataFrame(
-            rows4, columns=['traceability_code', 'cropname',
+            rows4, columns=['cropname',
                             'netweight', 'created_at', 'district']
         )
 
         data4['year'] = pd.to_datetime(data4['created_at']).dt.year
         data4['netweight'] = data4['netweight'].fillna(
-            0).astype(int)  # Fill NaN with 0 and convert to int
+            0).astype(int)
+
+    if rows5:
+        data5 = pd.DataFrame(
+            rows5, columns=['countryname', 'cropname',
+                            'weightreceived', 'weight', 'exportcompanyname']
+        )
+
+    if rows6:
+        data6 = pd.DataFrame(
+            rows6, columns=['cropname', 'weightreceived',
+                            'weight', 'countryname']
+        )
+        # Fill NaN with 0 and convert to int
 
 # Function to create CSV download link
 
@@ -223,6 +260,16 @@ with st.sidebar:
     selected_districts = st.multiselect(
         'Select districts', district_list, default=district_list)
 
+    # New filter for "Top Crop Exported by Country"
+    top_crop_list = list(data6['cropname'].unique())
+    selected_top_crops = st.multiselect(
+        'Select Top Crops for Exported by Country', top_crop_list, default=top_crop_list)
+
+    # New filter for "Top Export Companies"
+    export_company_list = list(data5['exportcompanyname'].unique())
+    selected_export_companies = st.multiselect(
+        'Select TopExport Companies', export_company_list, default=export_company_list)
+
     # Filtering data
     filter_data1 = data1[(data1['year'] == selected_year) &
                          (data1['status'].isin(selected_status))]
@@ -232,21 +279,40 @@ with st.sidebar:
                          (data3['cropname'].isin(selected_crops))]
     filter_data4 = data4[(data4['year'] == selected_year) &
                          (data4['district'].isin(selected_districts))]
+    # Filtering Top Crop Export Summary by selected crops and countries
+    filtered_crop_export_summary = data6[
+        (data6['cropname'].isin(selected_top_crops)) &
+        (data6['countryname'].isin(selected_countries))
+    ].sort_values(by='weightreceived', ascending=False)
 
-    st.markdown(combined_filedownload(filter_data1, filter_data2, filter_data3, filter_data4),
+    # Filtering Top Export Companies by selected crops and companies
+    filtered_top_exporters_agg = data5[
+        (data5['cropname'].isin(selected_top_crops)) &
+        (data5['countryname'].isin(selected_countries)) &
+        (data5['exportcompanyname'].isin(selected_export_companies))
+    ]
+
+    st.markdown(combined_filedownload(filter_data1, filter_data2, filter_data3, filter_data4, ),
                 unsafe_allow_html=True)
 
 # Calculate total weight for data1
 total_weight = filter_data1['product_weight'].sum()
-total_weight_str = f"{total_weight} Kg"
+total_weight_str = f"{total_weight:,.0f} Kg"
 # show the status of the filter
 status_weight = selected_status
 # Filter columns for filter_data2
 filter_data2 = filter_data2[[
     'country', 'clientcompany', 'product_weight']]
+# Group by country and sum the product weights
+country_weights = filter_data2.groupby('country')['product_weight'].sum()
+# Get the top 5 countries with the highest product_weight
+top_5_countries = country_weights.nlargest(5).index
+
+# Filter the original dataframe to only include rows from these top 5 countries
+filter_data2_top5 = filter_data2[filter_data2['country'].isin(top_5_countries)]
 
 # Main Panel
-col = st.columns((3, 5), gap='medium')
+col = st.columns((4, 5), gap='medium')
 
 with col[0]:
     st.markdown('#### Total Product Weight')
@@ -261,12 +327,12 @@ with col[0]:
     # Two line breaks for more space
     st.markdown("<br><br><br>", unsafe_allow_html=True)
 
-    st.markdown('#### Destinations')
+    st.markdown('#### Top 5 Country Destinations')
     # Create donut pie chart
-    pie = alt.Chart(filter_data2).mark_arc(innerRadius=50).encode(
+    pie = alt.Chart(filter_data2_top5).mark_arc(innerRadius=50).encode(
         theta=alt.Theta(field="product_weight", type="quantitative"),
-        color=alt.Color(field="clientcompany", type="nominal"),
-        tooltip=['clientcompany', 'country', 'product_weight']
+        color=alt.Color(field="country", type="nominal"),
+        tooltip=['country', 'clientcompany', 'product_weight']
     ).properties(
         width=400,
         height=400
@@ -274,12 +340,24 @@ with col[0]:
 
     st.altair_chart(pie, use_container_width=True)
 
-    st.markdown('### Top Districts')
+    st.markdown('### Top Export Districts')
 
-    filter_data4_sorted = filter_data4.sort_values(
+    # Aggregate the data by district and cropname, summing up the netweight for each group
+    filter_data4_sorted = filter_data4.groupby(['district', 'cropname'])[
+        'netweight'].sum().reset_index()
+
+    # Sort the aggregated data by netweight in descending order
+    filter_data4_sorted = filter_data4_sorted.sort_values(
         by='netweight', ascending=False)
+
+    # Check if the DataFrame is not empty before finding the max value
+    if not filter_data4_sorted.empty:
+        max_netweight = max(filter_data4_sorted['netweight'])
+    else:
+        max_netweight = 0  # Default value if the DataFrame is empty
+
     st.dataframe(filter_data4_sorted,
-                 column_order=("cropname", "district", "netweight", "year"),
+                 column_order=("district", "cropname", "netweight"),
                  hide_index=True,
                  width=None,
                  column_config={
@@ -290,15 +368,29 @@ with col[0]:
                          "District",
                      ),
                      "netweight": st.column_config.ProgressColumn(
-                         "Net Weight",
+                         "Net Weight(Kg)",
                          format="%d",
-                         min_value=0,
-                         max_value=max(filter_data4_sorted['netweight']),
-                     ),
-                     "year": st.column_config.TextColumn(
-                         "Year",
-                     )}
+                         min_value=0, max_value=20000,),
+                 }
                  )
+
+    st.markdown('### Top Export Companies')
+
+    # Aggregate the total exported weight for export companies
+    top_exporters_aggregated = data5.groupby(['exportcompanyname']).agg(
+        total_weight_received=('weightreceived', 'sum'), total_weight=('weight', 'sum')).reset_index()
+
+    top_exporters = top_exporters_aggregated.sort_values(
+        by='total_weight_received', ascending=False)
+
+    st.dataframe(top_exporters,
+                 column_order=("exportcompanyname",
+                               "total_weight_received"),
+                 hide_index=True,
+                 column_config={
+                     "exportcompanyname": st.column_config.TextColumn("Export Company"),
+                     "total_weight_received": st.column_config.ProgressColumn("Total Weight Received(Kg)", format="%d", min_value=0, max_value=50000,), })
+    #  "total_weight_exported": st.column_config.ProgressColumn("Total Exported Weight", format="%d", min_value=0), })
 
 with col[1]:
     st.markdown('#### Product Volumes Received Per Month')
@@ -341,6 +433,29 @@ with col[1]:
 
     st.altair_chart(reception_line_chart, use_container_width=True)
 
+    st.markdown('### Top Crop Exported')
+    # Aggregate the total weight and weight received for each crop name and country
+    crop_export_summary_aggregated = data6.groupby(['cropname', 'countryname']).agg(
+        total_weight_received=('weightreceived', 'sum'), total_weight=('weight', 'sum')).reset_index()
+
+    # Sort by total weight received, with the highest weight first
+    crop_export_summary_sorted = crop_export_summary_aggregated.sort_values(
+        by='total_weight_received', ascending=False)
+
+    # Display the sorted data
+    st.dataframe(crop_export_summary_sorted,
+                 column_order=("cropname", "countryname",
+                               "total_weight_received"),
+                 hide_index=True,
+                 use_container_width=True,
+                 column_config={
+                     "cropname": st.column_config.TextColumn("Crop Name"),
+                     "countryname": st.column_config.TextColumn("Country"),
+                     "total_weight_received": st.column_config.ProgressColumn("Weight for Export(Kg)", format="%d", min_value=0, max_value=20000,)
+
+                 }
+                 )
+
 
 # Close connection when the app is shut down
 
@@ -350,6 +465,6 @@ def close_connection():
         server.stop()
 
 
-if st.button("Shutdown Connection"):
-    close_connection()
-    st.session_state.shutdown = True
+# if st.button("Shutdown Connection"):
+#     close_connection()
+#     st.session_state.shutdown = True
